45f19e6ae7ffd60388b37e5f5cab0112bbd25e74
wissem chiha <111453233+wissem01chiha@users.noreply.github.com>
2023-05-07 22:48:35 +0100

    Update STM32F4_Servo_Motor_Control.c

--- STM32F4_Servo_Motor_Control.c
+++ STM32F4_Servo_Motor_Control.c
@@ -1,99 +1,18 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2022 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
- * @brief  The application entry point.
- * @retval int
- */
+ 
 int main(void) {
-	/* USER CODE BEGIN 1 */
-
-	/* USER CODE END 1 */
-
-	/* MCU Configuration--------------------------------------------------------*/
-
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
-
-	/* USER CODE BEGIN Init */
-
-	/* USER CODE END Init */
-
-	/* Configure the system clock */
-	SystemClock_Config();
-
-	/* USER CODE BEGIN SysInit */
-
-	/* USER CODE END SysInit */
-
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
+	
+	HAL_Init();					/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	SystemClock_Config();				/* Configure the system clock */
+	MX_GPIO_Init();					/* Initialize all configured peripherals */
 	MX_TIM1_Init();
-	/* USER CODE BEGIN 2 */
 	HAL_TIM_PWM_Start_IT(&htim1, TIM_CHANNEL_1);
 
-	/* USER CODE END 2 */
-
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
-	TIM1->ARR = 19999; //autoreload counter
+	TIM1->ARR = 19999; 			/*autoreload counter*/
 
 	while (1) {
 		/* to control a servo motor we need a PWM signal (pulse width modulation ) with a 50Hz frequency
@@ -104,30 +23,20 @@ int main(void) {
 		 * duty cycle of 0° is 0.05(1 ms /20 ms )
 		 * so our duty cycle will be between 0.05 and 0.1   */
 
-		TIM1->CCR1 = 1999; // duty cycle=0.0999 -->180°
-		HAL_Delay(1000); //delay of 1000ms
-		TIM1->CCR1 = 19; //duty cycle =0.075 --> 90°
+		TIM1->CCR1 = 1999; 		/* duty cycle=0.0999 -->180° */
+		HAL_Delay(1000); 		/* delay of 1000ms */ 
+		TIM1->CCR1 = 19; 		/* duty cycle =0.075 --> 90° */
 		HAL_Delay(1000);
-		TIM1->CCR1 = 998; // duty cycle =0.0499 -->0°
-		HAL_Delay(1000);
-
-		/* USER CODE END WHILE */
-
-		/* USER CODE BEGIN 3 */
-	}
-	/* USER CODE END 3 */
+		TIM1->CCR1 = 998; 		/* duty cycle =0.0499 -->0° */
+		HAL_Delay(1000); 
+	} 
 }
-
-/**
- * @brief System Clock Configuration
- * @retval None
- */
 void SystemClock_Config(void) {
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-	/** Configure the main internal regulator output voltage
-	 */
+	/* Configure the main internal regulator output voltage */
+	 
 	__HAL_RCC_PWR_CLK_ENABLE();
 	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
@@ -160,26 +69,13 @@ void SystemClock_Config(void) {
 		Error_Handler();
 	}
 }
-
-/**
- * @brief TIM1 Initialization Function
- * @param None
- * @retval None
- */
 static void MX_TIM1_Init(void) {
-
-	/* USER CODE BEGIN TIM1_Init 0 */
-
-	/* USER CODE END TIM1_Init 0 */
-
+	
 	TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
 	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
 	TIM_OC_InitTypeDef sConfigOC = { 0 };
 	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = { 0 };
 
-	/* USER CODE BEGIN TIM1_Init 1 */
-
-	/* USER CODE END TIM1_Init 1 */
 	htim1.Instance = TIM1;
 	htim1.Init.Prescaler = 15;
 	htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -225,55 +121,10 @@ static void MX_TIM1_Init(void) {
 			!= HAL_OK) {
 		Error_Handler();
 	}
-	/* USER CODE BEGIN TIM1_Init 2 */
-
-	/* USER CODE END TIM1_Init 2 */
 	HAL_TIM_MspPostInit(&htim1);
-
 }
-
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
 static void MX_GPIO_Init(void) {
-
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOE_CLK_ENABLE();
-
 }
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1) {
-	}
-	/* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+ 
